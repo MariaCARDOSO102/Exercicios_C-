573b6ff6387654ffc09a62e0486eeea9ee34a431
MariaCARDOSO102 <mariaclarab.cardoso123@gmail.com>
2024-05-24 13:15:39 -0300

    Subir projeto

---
+++ Biblioteca/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        { 
+        }
+
+        // aqui são criadas as tabelas 
+        public DbSet<EmprestimoModel> Emprestimos { get; set; }
+    }
+}
